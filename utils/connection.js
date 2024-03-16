d0dc837c4530e673a0badcc86f89e386123848ac
Sumair-Aslam <80763643+Sumair-Aslam@users.noreply.github.com>
2024-03-17 01:26:34 +0500

    Update connection.js

--- utils/connection.js
+++ utils/connection.js
@@ -1,11 +1,6 @@
 const config = require("config");
 const { Sequelize } = require("sequelize");
 const { DATABASE, USERNAME, PASSWORD, HOST, PORT } = config.get("DBCONFIG");
-console.log("Databse: ", DATABASE)
-console.log("Host: ", HOST)
-console.log("PASSWORD: ", PASSWORD)
-console.log("PORT: ", PORT)
-console.log("USERNAME: ", USERNAME)
 
 module.exports = new Sequelize(DATABASE, USERNAME, PASSWORD, {
   host: HOST,
